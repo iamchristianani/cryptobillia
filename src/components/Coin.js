7910e0526d5371e9ca7a5ebc4937d59cae4f5198
Christian Ani <iamchristianani@gmail.com>
2023-02-07 14:46:45 +0100

    Update Coin component to display each coin

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import { BsArrowRightCircle } from "react-icons/bs";
 
-const Coin = () => (
-  <div className="eachCoin">
-    <div className="backIcon">Back</div>
-    <div className="coinInfo">
-      <div className="coinName">BTC</div>
-      <div className="coinPrice">$39389</div>
-    </div>
-  </div>
-);
+const Coin = ({id, name, market_cap, current_price, total_volume, image}) => {
+
+  return (
+    <>
+      <li className="eachCoin">
+        <div className="imgNdIcon">
+          <div className="coinImage">
+            <img src={image} alt={id} />
+          </div>
+          <div className="backIcon">
+            <BsArrowRightCircle />
+          </div>
+        </div>
+        <div className="coinInfo">
+          <div className="coinName">Name: {name}</div>
+          <div className="coinPrice">Current Price: {current_price}</div>
+          <div className="marketCap">Market Cap: {market_cap}</div>
+          <div className="totalVolume">Total Volume: {total_volume}</div>
+        </div>
+      </li>
+    </>
+  )
+};
 
 export default Coin;
