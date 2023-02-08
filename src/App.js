d9b089ce882b72ffee5bdf3808b4d9479d37363b
Christian Ani <iamchristianani@gmail.com>
2023-02-08 15:04:45 +0100

    Import Route and Details and add Router for each

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Home from './components/Home';
+import Home from './pages/Home';
+import Details from './pages/Details';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/details/:coinId" element={<Details />} />
       </Routes>
       <Footer />
     </div>
