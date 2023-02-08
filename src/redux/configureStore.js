0fe43b1082bd4a04f31ee836e6139115aa003f06
Christian Ani <iamchristianani@gmail.com>
2023-02-08 15:09:33 +0100

    Create coin action and reducer

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,11 +1,13 @@
 import { configureStore, applyMiddleware } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
 import coinsReducer from './coinlist/coinlist';
+import detailsReducer from './coindetails/coindetails';
 
 const store = configureStore(
   {
     reducer: {
       coins: coinsReducer,
+      details: detailsReducer,
     },
   },
   applyMiddleware(thunk),
