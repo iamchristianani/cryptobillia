c79cf0ba4b05039dac8998d9706cc82beba87711
Christian Ani <iamchristianani@gmail.com>
2023-02-08 15:06:19 +0100

    import and use useLocation and NavLink

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,31 @@
 import React from 'react';
+import { useLocation, NavLink } from 'react-router-dom';
 import { AiOutlineSetting } from 'react-icons/ai';
 import { BiMicrophone } from 'react-icons/bi';
 import { IoIosArrowBack } from 'react-icons/io';
+import logoImg from '../assets/logo.png';
 
-const Header = () => (
-  <nav>
-    <div className="backIcon">
-      <IoIosArrowBack />
-    </div>
-    <div className="pageTitle">
-      <p>Cryptobillia</p>
-    </div>
-    <div className="navIcons">
-      <BiMicrophone />
-      <AiOutlineSetting />
-    </div>
-  </nav>
-);
+const Header = () => {
+  const location = useLocation();
+
+  return (
+    <nav>
+      {location.pathname !== '/' && (
+        <div className="headerBackIcon">
+          <NavLink to="/">
+            <IoIosArrowBack />
+          </NavLink>
+        </div>
+      )}
+      <div className="pageTitle">
+        <img src={logoImg} alt="logo" />
+      </div>
+      <div className="navIcons">
+        <BiMicrophone />
+        <AiOutlineSetting />
+      </div>
+    </nav>
+  );
+};
 
 export default Header;
